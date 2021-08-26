5a60c02cba80553e3c945acaa615c38cc80fa721
klich404 <klich4010@gmail.com>
2021-08-26 16:18:17 -0500

    task 0 - i only want to cry

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,9 +22,9 @@ sudo chown -R ubuntu:ubuntu /data/
 #update nginx configuration
 sudo echo 'Holberton School' |sudo tee /var/www/html/index.nginx-debian.html
 sudo echo "Ceci n'est pas une page" |sudo tee /usr/share/nginx/html/custom_404.html
-sudo sed -i "/listen 80 default_server/a \\\terror_page 404 /custom_404.html;\n\tlocation = /custom_404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t\t}" /etc/nginx/sites-available/default
-sudo sed -i "/listen 80 default_server/a \\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default
-sudo sed -i "/:80 default_server/ a \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+sudo sed -i "/:80 default_server/a \\\terror_page 404 /custom_404.html;\n\tlocation = /custom_404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t\t}" /etc/nginx/sites-available/default
+sudo sed -i "/:80 default_server/a \\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default
+sudo sed -i "/:80 default_server/ a \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" /etc/nginx/sites-available/default
 
 #restart nginx
 sudo service nginx restart
