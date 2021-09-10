0b181239c456ca8d189ca9f45ce527c29c988a59
klich404 <klich4010@gmail.com>
2021-09-10 16:36:22 -0500

    file and db storage updated

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -68,3 +68,7 @@ class DBStorage:
         Session = scoped_session(sessionmaker(
             bind=self.__engine, expire_on_commit=False))
         self.__session = Session()
+
+    def close(self):
+        """close session"""
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,3 +61,7 @@ class FileStorage:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             if key in self.__objects:
                 del self.__objects[key]
+
+    def close(self):
+        """deserializing the JSON file to objects"""
+        self.reload()
